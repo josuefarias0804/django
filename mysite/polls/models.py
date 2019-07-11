da27f357e2223aeada9ec8f3a238835371566d9c
josuefarias0804 <51660099+josuefarias0804@users.noreply.github.com>
2019-07-11 01:31:02 -0300

    Add files via upload

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,14 @@
-from datetime import timezone, datetime
+from django.utils import timezone
+from datetime import datetime
+
 
 from django.db import models
 import json
 
 
+
 class Question(models.Model):
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -17,11 +21,23 @@ class Question(models.Model):
     was_published_recently.short_description = 'Published recently?'
 
     def to_json(self):
-        my_dict = [{self.id: 1, "choice_text": "opcion", "votes": 2}, {self.id: 2, "choice_text": ""}]
+        choice_text_tojson = Choice.objects.filter(question__pub_date__year=timezone.now().year)
+        my_dict = [{self.id: 1, "choice_text": choice_text_tojson, "votes": 2}, {self.id: 2, "choice_text": choice_text_tojson}]
         timestampStr = self.pub_date.strftime("%d-%b-%Y (%H:%M:%S.%f)")
         return json.dumps({"id": 1, "question_text": self.question_text, "pub_date": timestampStr, "choices": str(my_dict)})
 
+    def post(self):
+        
+        data = {
+            'ids': [12, 3, 4, 5, 6]
+        }
 
+        req = urllib2.Request('http://example.com/api/posts/create')
+        req.add_header('Content-Type', 'application/json')
+
+        response = urllib2.urlopen(req, json.dumps(data))
+
+        return response
 
 class Choice(models.Model):
 
@@ -30,7 +46,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return f'{self.id}: {self.choice_text} {self.question} {self.votes}'
+       return f'{self.id}: {self.choice_text} {self.question} {self.votes}'
 
     def to_json(self):
         return json.dumps({"id": self.id, "choice_text": self.choice_text, "votes": self.votes})
