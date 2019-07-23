6c477a97bc94e988e1b5a306854ab62525845513
josuefarias0804 <51660099+josuefarias0804@users.noreply.github.com>
2019-07-23 01:57:59 -0300

    Add files via upload

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,6 @@
 from django.http import StreamingHttpResponse
 from django.utils import timezone
-from datetime import datetime
+from datetime import datetime, timedelta
 from urllib.request import urlopen
 
 
@@ -25,9 +25,11 @@ class Question(models.Model):
 
     def to_json(self):
         # choice_text_tojson = Choice.objects.filter(question__pub_date__year=timezone.now().year)
-        my_dict = [{self.id, "choice_text": choice_text_tojson, "votes": 2}, {self.id: 2, "choice_text": choice_text_tojson}]
+        # my_dict = [{self.id, "choice_text": choice_text_tojson, "votes": }, {self.id, "choice_text": choice_text_tojson}]
+        # a = Question(id=None, question_text="This is a test", pub_date=date(2005, 7, 27), choice=c)
+
         timestampStr = self.pub_date.strftime("%d-%b-%Y (%H:%M:%S.%f)")
-        return json.dumps({"id": self.id, "question_text": self.question_text, "pub_date": timestampStr, "choices": str(my_dict)})
+        return json.dumps({"id": self.id, "question_text": self.question_text, "pub_date": timestampStr, "choices": self.choice })
 
 
 
@@ -36,6 +38,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    choice = (question, choice_text, votes)
 
     def __str__(self):
        return f'{self.id}: {self.choice_text} {self.question} {self.votes}'
